b35bdb6bc3f8a787f422292dde495a3ba428ac9e
malamsyah <20480136+malamsyah@users.noreply.github.com>
2026-05-06 18:38:02 +0900

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.go
@@ -0,0 +1,26 @@
+func longestConsecutive(nums []int) int {
+    set := make(map[int]struct{})
+
+    for _, v := range nums {
+        set[v] = struct{}{}
+    }
+
+    res := 0
+    for _, v := range nums {
+        curr := v
+        a := 1
+        for {
+            if _, found := set[curr + 1]; found {
+                curr++
+                a++
+            } else {
+                break
+            }
+        }
+        if a > res {
+            res = a
+        }
+    }
+
+    return res
+}
